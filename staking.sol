5642c295cd13ac4b05c9cea63ee6c81fcdc5b46f
Hooman Radmehr <79998469+HoomanRadmehr@users.noreply.github.com>
2023-07-12 19:44:39 +0330

    Update staking.sol

--- staking.sol
+++ staking.sol
@@ -11,8 +11,8 @@ interface IGenealogyContract {
 
 contract StakingContractFactory is Ownable {
     address[] public contracts;
-
-    event ContractCreated(address contractAddress);
+    // StakingContract[] public stakingContracts;
+    event StakingContractCreated(address contractAddress);
 
     struct ContractDetails {
         address contractAddress;
@@ -40,7 +40,7 @@ contract StakingContractFactory is Ownable {
     function newStakingContract(address _token, uint _duration, uint _rewardDuration, uint _minDeposit, uint _maxDeposit, uint _rewardRatio) public onlyOwner returns(address newContract) {
         StakingContract c = new StakingContract(_token, msg.sender, _duration, _rewardDuration, _minDeposit, _maxDeposit, _rewardRatio);
         contracts.push(address(c));
-        emit ContractCreated(address(c));
+        emit StakingContractCreated(address(c));
         return address(c);
     }
 
@@ -50,6 +50,12 @@ contract StakingContractFactory is Ownable {
             totalRewards += StakingContract(contracts[i]).getReward(_user);
         }
         return totalRewards;
+    }
+
+    function issueAllRewards() public onlyOwner {
+        for(uint256 i = 0; i < contracts.length; i++) {
+            StakingContract(contracts[i]).issueRewards();
+        }
     }
 
     function setGenealogyContract(address _genealogyContract) public onlyOwner {
@@ -98,6 +104,7 @@ contract StakingContractFactory is Ownable {
 }
 
 contract StakingContract is Ownable {
+
     IERC20 public token;
     address public creator;
     address public genealogyContractAddress;
@@ -110,12 +117,19 @@ contract StakingContract is Ownable {
     mapping(address => uint) public stakingStartTimes;
     mapping(address => uint) public rewardIssueTimes;
     mapping(address => uint) public totalRewards;
-    mapping(address => bool) public stakers;
+    address[] public stakers;
     address[] public users;
 
     event Stake(address indexed user, uint amount);
-    event RewardIssued(address indexed user, uint amount);
-    event StakeCanceled(address indexed user, uint refund);
+    event Withdraw(address indexed user, uint256 amount);
+    event RewardIssued(address indexed user, uint256 reward);
+
+    struct StakeDetail {
+        uint256 timestamp;
+        uint256 amount;
+    }
+
+    mapping(address => StakeDetail[]) public stakingDetails;
 
     constructor(address _token, address _owner, uint _duration, uint _rewardDuration, uint _minDeposit, uint _maxDeposit, uint _rewardRatio) {
         token = IERC20(_token);
@@ -127,14 +141,14 @@ contract StakingContract is Ownable {
         rewardRatio = _rewardRatio;
     }
 
-    function stake(uint _amount) public {
+    function stakeTokens(uint256 _amount) public {
         require(_amount >= minDeposit && _amount <= maxDeposit, "Invalid deposit amount");
-        stakes[msg.sender] += _amount;
-        stakingStartTimes[msg.sender] = block.timestamp;
-        rewardIssueTimes[msg.sender] = block.timestamp;
         token.transferFrom(msg.sender, address(this), _amount);
-        users.push(msg.sender);
-        stakers[msg.sender] = true;
+        if(stakes[msg.sender] == 0) {
+            stakers.push(msg.sender);
+        }
+        stakes[msg.sender] += _amount;
+        stakingDetails[msg.sender].push(StakeDetail(block.timestamp, _amount));
         emit Stake(msg.sender, _amount);
     }
 
@@ -146,15 +160,18 @@ contract StakingContract is Ownable {
         return totalRewards[_user];
     }
 
-    function issueReward() public onlyOwner {
-        for(uint i = 0; i < users.length; i++) {
-            if(block.timestamp >= rewardIssueTimes[users[i]] + rewardDuration 
-               && block.timestamp < stakingStartTimes[users[i]] + stakingDuration) {
-                uint reward = stakes[users[i]] * rewardRatio / 100;
-                totalRewards[users[i]] += reward;
-                token.transfer(users[i], reward);
-                rewardIssueTimes[users[i]] = block.timestamp;
-                emit RewardIssued(users[i], reward);
+    function issueRewards() public onlyOwner {
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address staker = stakers[i];
+            for (uint256 j = 0; j < stakingDetails[staker].length; j++) {
+                StakeDetail storage stakeDetail = stakingDetails[staker][j];
+                if (block.timestamp > stakeDetail.timestamp + rewardDuration) {
+                    uint256 reward = (stakeDetail.amount * rewardRatio) / 100;
+                    totalRewards[staker] += reward;
+                    token.transfer(staker, reward);
+                    stakeDetail.timestamp = block.timestamp;
+                    emit RewardIssued(staker, reward);
+                }
             }
         }
     }
@@ -163,21 +180,25 @@ contract StakingContract is Ownable {
         genealogyContractAddress = _genealogyContract;
     }
 
-    function cancelStake() public {
-        require(stakes[msg.sender] > 0, "No stake to withdraw");
-        require(block.timestamp > stakingStartTimes[msg.sender] + 90 days, "Cannot cancel staking before 3 months");
-        IGenealogyContract genealogyContract = IGenealogyContract(genealogyContractAddress);
-        require(!genealogyContract.hasChild(msg.sender), "User with children cannot cancel staking");
-        uint refund = stakes[msg.sender] > totalRewards[msg.sender] ? stakes[msg.sender] - totalRewards[msg.sender] : 0;
+    function withdrawTokens() public {
+        require(stakes[msg.sender] > 0, "No stakes found");
+        token.transfer(msg.sender, stakes[msg.sender]);
+        emit Withdraw(msg.sender, stakes[msg.sender]);
+        for (uint256 i = 0; i < stakers.length; i++) {
+            if (stakers[i] == msg.sender) {
+                stakers[i] = stakers[stakers.length - 1];
+                stakers.pop();
+                break;
+            }
+        }
         stakes[msg.sender] = 0;
-        totalRewards[msg.sender] = 0;
-        stakers[msg.sender] = false;
-        token.transfer(msg.sender, refund);
-        emit StakeCanceled(msg.sender, refund);
     }
 
     function isStaker(address _address) public view returns(bool) {
-        return stakers[_address];
+        if (stakes[_address]>0){
+            return true;
+        }
+        else return false;
     }
 
     function getAllUsers() public view returns (address[] memory) {
